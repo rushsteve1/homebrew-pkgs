6de24735657c9ab4ad4e05a3819be588b13fdb31
Steven vanZyl <rushsteve1@rushsteve1.us>
2023-08-24 12:27:47 -0400

    Add trash-d package

---
+++ Formula/trash-d.rb
@@ -0,0 +1,25 @@
+class TrashD < Formula
+  desc "Near drop-in replacement for rm that uses the trash bin"
+  homepage "https://github.com/rushsteve1/trash-d"
+  url "https://github.com/rushsteve1/trash-d/archive/refs/tags/19.tar.gz"
+  sha256 "9eae6eb35d86e1b388a4495bd32e21bd257fcd02635430f4bb03a134b85ceeff"
+  license "MIT"
+
+  depends_on "dub" => :build
+  depends_on "ldc" => :build
+  depends_on "ronn" => :build
+
+  def install
+    system "dub", "build", "--build=release"
+    system "strip", "./build/trash"
+    bin.install "./build/trash"
+
+    system "ronn", "--roff", "--pipe", "MANUAL.md", ">", "trash.1"
+    man.mkpath
+    man1.install "trash.1"
+  end
+
+  test do
+    assert_match(/trash-d version #{version}.*/, shell_output("#{bin}/trash --version"))
+  end
+end
