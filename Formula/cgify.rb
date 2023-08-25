3224cb82e67650471ce74a09e020083d26590a9f
Steven vanZyl <rushsteve1@rushsteve1.us>
2023-08-25 10:41:51 -0400

    Cgify formula and fixes (#1)
    * Cgify formula
    
    And some minor tweaks to the trash-d tests
    
    * Cgify formula
    
    And some minor tweaks to the trash-d tests
    
    * Audit cgify and mention dev branch in readme
    
    * Fix trash-d package

---
+++ Formula/cgify.rb
@@ -0,0 +1,21 @@
+class Cgify < Formula
+  desc "Dead simple HTTP/FCGI to CGI proxy"
+  homepage "https://github.com/rushsteve1/cgify"
+  url "https://github.com/rushsteve1/cgify/archive/refs/tags/1.tar.gz"
+  sha256 "2b54302494dddc215565473971337f156b763c8dfe095fdb16cf5898df00b0ea"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    # We expect running it with no arguments to error
+    require "open3"
+    _, err, status = Open3.capture3 "#{bin}/cgify"
+    assert !status.success?
+    assert_match(/CGI directory/, err)
+  end
+end
--- README.md
+++ README.md
@@ -6,6 +6,9 @@ These should all be mostly stable, but use at your own risk.
 
 Pre-built binary Bottles should be produced for all packages, if you prefer.
 
+Actual development happens on the `dev` branch, in order to keep `main` a bit
+more stable.
+
 ## How do I install these formulae?
 
 `brew install rushsteve1/pkgs/<formula>`
